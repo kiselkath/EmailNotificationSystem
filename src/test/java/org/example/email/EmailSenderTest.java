0f68e412376be2e297a8c7d1647650736d64bff6
katekiselevich <kiselkath@gmail.com>
2025-05-15 13:06:11 +0200

    Created more tests

--- src/test/java/org/example/email/EmailSenderTest.java
+++ src/test/java/org/example/email/EmailSenderTest.java
@@ -2,7 +2,7 @@ package org.example.email;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 /**
@@ -15,4 +15,19 @@ public class EmailSenderTest {
         assertDoesNotThrow(
                 () -> sender.run(), "EmailSender run method should not throw an exception.");
     }
+
+    @Test
+    public void threadShouldHaveCorrectName(){
+        Thread thread = new Thread(new EmailSender("test@example.com"), "EmailThread-1");
+        assertEquals("EmailThread-1", thread.getName(),"Thread name should follow the template '\"EmailThread-number\"");
+    }
+
+    @Test
+    public void mainThreadShouldWaitForEmailThreadToFinish() throws InterruptedException{
+        Thread thread = new Thread(new EmailSender("wait@example.com"));
+        thread.start();
+        thread.join();
+        assertFalse(thread.isAlive(), "Thread should be dead after join()");
+    }
+
 }
